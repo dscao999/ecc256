8bbc782e6792321138a82d794fdd21dc5de458c7
Dashi Cao <dscao999@gmail.com>
2019-10-22 19:43:49 +0800

    Add mulithreading to verify.py

--- verify.py
+++ verify.py
@@ -2,6 +2,7 @@
 #
 import os
 import sys
+import threading
 
 if len(sys.argv) > 1:
     dropfile = sys.argv[1]
@@ -9,25 +10,47 @@ else:
     print("Missing file name!")
     quit()
 
+numcpus = os.cpu_count()
+
+class ProcFile(threading.Thread):
+    def __init__(self, name, fname):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.fname = fname
+
+    def run(self):
+        print("Processing file: ", self.fname)
+        rs1 = os.popen("./ripe -f " + self.fname).read().split()
+        rs2 = os.popen("openssl dgst -ripemd160 " + self.fname).read().split()
+        if rs1[0] != rs1[2]:
+            print("Bad base64 for: ", self.fname)
+        if rs1[0] != rs2[1]:
+            print("Conflicting ripemd160 digest: ", self.fname)
+
+        rs1 = os.popen("./sha " + self.fname).read().split()
+        if len(rs1) == 4 and rs1[1] != rs1[3]:
+            print("File and string mode differ: ", self.fname)
+        rs2 = os.popen("openssl dgst -sha256 " + self.fname).read().split()
+        if rs1[1] != rs2[1]:
+            print("Conflicting sha256 digest: ", self.fname)
+
 fin = open(dropfile, "r")
 
+thlist = []
 count = 0
 for line in fin:
     fname = line.rstrip();
-    rs1 = os.popen("./ripe -f " + fname).read().split()
-    rs2 = os.popen("openssl dgst -ripemd160 " + fname).read().split()
-    if rs1[0] != rs1[2]:
-        print("Bad base64 for: ", fname)
-    if rs1[0] != rs2[1]:
-        print("Conflicting ripemd160 digest: ", fname)
-
-    rs1 = os.popen("./sha " + fname).read().split()
-    if len(rs1) == 4 and rs1[1] != rs1[3]:
-        print("File and string mode differ: ", fname)
-    rs2 = os.popen("openssl dgst -sha256 " + fname).read().split()
-    if rs1[1] != rs2[1]:
-        print("Conflicting sha256 digest: ", fname)
+    mth = ProcFile("check" + str(count), fname)
+    thlist.append(mth)
+    mth.start()
+    if len(thlist) > numcpus + 1:
+        mth = thlist[0]
+        del thlist[0]
+        mth.join()
     count += 1
 
-print("Total ", count, " files checked.")
 fin.close()
+for mth in thlist:
+    mth.join()
+
+print("Total ", count, " files checked.")
