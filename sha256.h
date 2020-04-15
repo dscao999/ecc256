4ddfa33625689d9625896ec923deb6a66a217ef6
Dashi Cao <dscao999@gmail.com>
2020-04-15 21:15:59 +0800

    Change sha256 digest to big endian integers

--- sha256.h
+++ sha256.h
@@ -6,6 +6,7 @@
 #define SHA256_DSCAO__
 #include <stdio.h>
 #include <stdlib.h>
+#include "loglog.h"
 
 #define SHA_BLOCK_LEN	64
 #define SHA_DGST_LEN	32
@@ -35,5 +36,23 @@ static inline void sha256_exit(struct sha256 *sha)
 void sha256(struct sha256 *hd, const unsigned char *buf, unsigned long len);
 
 void sha256_file(struct sha256 *hd, FILE *fin);
+
+static inline void sha256_to_str(unsigned char str[SHA_DGST_LEN],
+		const unsigned char *buf, unsigned long len)
+{
+	struct sha256 sha;
+
+	sha256_reset(&sha);
+	sha256(&sha, buf, len);
+	sha.H[0] = swap32(sha.H[0]);
+	sha.H[1] = swap32(sha.H[1]);
+	sha.H[2] = swap32(sha.H[2]);
+	sha.H[3] = swap32(sha.H[3]);
+	sha.H[4] = swap32(sha.H[4]);
+	sha.H[5] = swap32(sha.H[5]);
+	sha.H[6] = swap32(sha.H[6]);
+	sha.H[7] = swap32(sha.H[7]);
+	memcpy(str, sha.H, SHA_DGST_LEN);
+}
 
 #endif  /* SHA256_DSCAO__ */
